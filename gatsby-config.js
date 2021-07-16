1eabbd409e6c9ed3ce0ce4558beeb5b4970f4223
Mansi <mansi.budhiraja@gmail.com>
2021-07-16 16:21:46 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "ente",
+  },
+  plugins: ["gatsby-plugin-netlify-cms", "gatsby-plugin-gatsby-cloud"],
+};

c32dcd4bbeee6c09a0802b7e7bf54361728247b4
Mansi <mansi.budhiraja@gmail.com>
2021-08-09 20:25:56 -0700

    made changes in svg files

--- src/components/price.js
+++ src/components/price.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PriceDetails from './PriceDetails'
 import styled from "styled-components"
-// import {CSSTransition} from 'react-transition-group'
-// import '../components/layout/layout.css'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+import '../components/layout/layout.css'
+
 
 
 const FreeTrial = styled.div `
@@ -11,6 +12,7 @@ const FreeTrial = styled.div `
     padding-top: 28px;
     color: rgb(120, 120, 120);
 `;
+
 
 
 class Price extends React.Component{
@@ -85,7 +87,9 @@ class Price extends React.Component{
                             </div>
                             <div className="container" style={{marginTop: "12px"}}>
                                 <div id="pricing-plans-monthly" className="row">
+                                <TransitionGroup >
                                     {this.state.plans.map((plan, index) => (
+                                    <CSSTransition timeout={1000} classNames="fade" key={plan.id}>
                                         <PriceDetails key={plan.id} 
                                             storage={this.convert(plan.storage)} 
                                             price={plan.price} 
@@ -93,10 +97,11 @@ class Price extends React.Component{
                                             { index !== this.state.plans.length - 1 
                                             ? <div className="col-auto pricing-plan-separator align-self-center"/> : null }
                                         </PriceDetails> 
+                                    </CSSTransition>
                                     ))} 
+                                </TransitionGroup>
                                 </div>
                             </div>
-    
                             <div className="row justify-content-center pricing-plan-duration-toggle">
                                 <div id="pricing-plan-duration-monthly" className="col-auto pricing-plan-duration pricing-plan-duration-selected">
                                     monthly
@@ -108,6 +113,7 @@ class Price extends React.Component{
                                     yearly
                                 </div>
                             </div>
+                          
                             <FreeTrial>
                                 we offer a free trial of <strong id="free_plan_storage">1 GB</strong> for <strong id="free_plan_duration">365 days</strong>
                             </FreeTrial>
